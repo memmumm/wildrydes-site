601488d6e4cee12d8e1d02a6f6298eed00725e10
MerviGit <mervi.siipilahde@gmail.com>
2020-08-13 15:34:15 +0300

    updated the website config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_dyiLxSa5i', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2au9kmpstgp0e5nrh0doh2538v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
